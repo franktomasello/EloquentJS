e0fdfde36dda83ade7be72368e9c771bb86634e6
ftomasello <32821413+franktomasello@users.noreply.github.com>
2019-12-01 18:15:01 -0800

    added chessboard exercise 3

---
+++ ChessBoard.js
@@ -0,0 +1,14 @@
+let pound = "#";
+let count = 8;
+
+for (let i = 0; i < count; i ++)
+{
+    if( i % 2 === 0)
+    {
+        console.log("# # # # ");
+    }
+    else
+    {
+        console.log(" # # # #")
+    }
+}
